ca13a0cd48cc9893b033dfc998623bcd0ced3599
nati schmidt <nati.schmidt97@outlook.de>
2023-04-16 13:18:55 +0200

    some css

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import "./Timer.css";
 
 export default function Timer(props: { time: number; }) {
     return (
         <div className="timer">
-      <span className="display">
-
+      <span className="display min">
         {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
       </span>
-            <span className="display">
+            <span className="display sec">
         {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}.
       </span>
             <span className="display mili-sec">
